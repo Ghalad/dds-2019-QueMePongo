584435a4e7d3d3e6739abdec9d0f738bc18351aa
Manuel Nielsen <manurocck22@gmail.com>
2019-05-10 16:54:20 -0300

    Create Regla1Superior.cs
    - Agrego regla que determina que no puede añadirse una prenda cuando ya hay una superior.
    
    - Puede más adelante adaptarse a dos superiores

---
+++ QueMePongo/QMP.Lib/Entidades/Reglas/Regla1Superior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ar.UTN.QMP.Lib.Entidades.Reglas
+{
+    class Regla1Superior : Regla //puede cambiarse luego a 2 superiores anidando un foreach más
+    {
+        public bool Validar(Atuendo atuendo)
+        {
+            int prendaAux;
+            Caracteristica caractSuperior = new Caracteristica("Zona", "Superior");
+
+            foreach (Prenda p in atuendo.Prendas)
+                if (p.tieneCaracteristica(caractSuperior) )
+                {
+                    prendaAux = p;
+                    foreach (Prenda pr in atuendo.Prendas)
+                    {
+                        if (p.tieneCaracteristica(caractSuperior) && pr.esDistintaA(p))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            return true;
+        }
+    }
+}
