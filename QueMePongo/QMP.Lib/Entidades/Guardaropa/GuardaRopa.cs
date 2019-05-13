58e52c82d69ce9fd8b2ee8d673b00704a46b4815
Manuel Nielsen <manurocck22@gmail.com>
2019-05-13 16:05:05 -0300

    Agrego comentarios y una clase sin testear
    - Estaría bueno que agregues algunos comentarios así conozco tu intención en cuanto al algoritmo sino tengo que deducir el código y me cuesta un poco

--- QueMePongo/QMP.Lib/Entidades/Guardaropa/GuardaRopa.cs
+++ QueMePongo/QMP.Lib/Entidades/Guardaropa/GuardaRopa.cs
@@ -6,5 +6,11 @@ namespace Ar.UTN.QMP.Lib.Entidades.Guardaropa
     public class GuardaRopa
     {
         public List<Atuendo> Atuendos;
+
+        //public List<Prenda> Prendas; 
+        //public Atuendo UnAtuendo;
+        // Me parece mas apropiado que el guardarropa guarde el conjunto de prendas totales que tiene
+        // y que los atuendos sea algo dinámico que se vaya definiendo según la combinación de prendas
+
     }
 }
