f4317f36f4c8ccd4bde75496ab89425989e43091
Guido Dicomo <gdicomo@afip.gob.ar>
2019-06-20 18:53:24 -0300

    Se agregan clases para el manejo de datos del clima

---
+++ QueMePongo/QMP.Lib/Entidades/Clima/OpenWeatherService.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace Ar.UTN.QMP.Lib.Entidades.Clima
+{
+    public class OpenWeatherService
+    {
+        private WeatherInfo WeatherInfo { get; set; }
+        private string AppId { get; set; }  //"8ed5caa2f1d3f297ff245132e0235d16";
+        private string Ciudad { get; set; } //"Buenos Aires";
+        private string @Url { get; set; }
+        private const int TIEMPO_DE_OBSOLENCIA = 1;
+
+
+        #region CONSTRUCTOR
+        public OpenWeatherService(string appid, string ciudad)
+        {
+            this.AppId = appid;
+            this.Ciudad = ciudad;
+            this.Url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&mode=json&units=metric&APPID={1}", this.Ciudad, this.AppId);
+        }
+        #endregion CONSTRUCTOR
+
+        #region PUBLICO
+        public decimal ObtenerHumedad()
+        {
+            this.RefrescarSiDatosObsoletos();
+            try
+            {
+                return Decimal.Parse(this.WeatherInfo.Main.Humidity);
+            }
+            catch
+            {
+                //TODO MANEJO DE EXCEPCIONES
+                throw new InvalidCastException();
+            }
+        }
+
+        public decimal ObtenerPresion()
+        {
+            this.RefrescarSiDatosObsoletos();
+            try
+            {
+                return Decimal.Parse(this.WeatherInfo.Main.Pressure);
+            }
+            catch
+            {
+                //TODO MANEJO DE EXCEPCIONES
+                throw new InvalidCastException();
+            }
+        }
+
+        public decimal ObtenerTemperatura()
+        {
+            this.RefrescarSiDatosObsoletos();
+            try
+            {
+                return Decimal.Parse(this.WeatherInfo.Main.Temp.Replace('.',','));
+            }
+            catch
+            {
+                //TODO MANEJO DE EXCEPCIONES
+                throw new InvalidCastException();
+            }
+        }
+        #endregion PUBLICO
+
+
+        #region PRIVADO
+        /// <summary>
+        /// Llama al servicio de Open Weather API y obtiene internamente la informacion del clima
+        /// </summary>
+        private void Refrescar()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(this.Url);
+                this.WeatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(json);
+            }
+        }
+
+        /// <summary>
+        /// Evalua si es necesario llama al servicio de Open Weather API para actualizar los datos del clima
+        /// </summary>
+        private void RefrescarSiDatosObsoletos()
+        {
+            if (this.WeatherInfo == null)
+                this.Refrescar();
+            else
+            {
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                dt = dt.AddSeconds(Int32.Parse(this.WeatherInfo.dt)).ToLocalTime(); // fecha de ultima obtencion de datos
+                dt = dt.AddMinutes(TIEMPO_DE_OBSOLENCIA);
+
+                if (dt.CompareTo(DateTime.Now) > 0)
+                    this.Refrescar();
+            }
+        }
+        #endregion PRIVADO
+
+    }
+}
---
+++ QueMePongo/QMP.Lib/Entidades/Clima/WeatherInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Ar.UTN.QMP.Lib.Entidades.Clima
+{
+    public class WeatherInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Weather> Weather { get; set; }
+        public string Visibility { get; set; }
+        public string dt { get; set; }
+        public Main Main { get; set; }
+        public Wind wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public Coord Coord { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+        public string Temp_min { get; set; }
+        public string Temp_max { get; set; }
+    }
+
+    public class Wind
+    {
+        public string Speed { get; set; }
+        public string Deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public string All { get; set; }
+    }
+
+    public class Coord
+    {
+        public string Lon { get; set; }
+        public string Lat { get; set; }
+    }
+}
